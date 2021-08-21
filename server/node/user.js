8e8b793987d2733c48115b8bce0b420537726241
nhhung1810 <59091317+yauangon@users.noreply.github.com>
2021-08-21 17:17:11 +0700

    intit some user and ready for testing
    - need autobot
    - 'jsonize' responses
    - add freeMoney function to init more money at the start

--- server/node/user.js
+++ server/node/user.js
@@ -1,6 +1,7 @@
-// const crypto = require("crypto")
+const crypto = require("crypto")
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
+const {Transaction, Blockchain} = require('./blockchain.js')
 
 class User{
     constructor(username, hashedPass){
@@ -14,6 +15,7 @@ class User{
 class UserFactory{
     constructor(){
         this.userList = []
+        this.initUser()
     }
 
     makeUser(username, hashedPass){
@@ -22,10 +24,38 @@ class UserFactory{
                 throw new Error("Existed this member")
             }
         })
-        tmp = new User(username, hashedPass)
+        let tmp = new User(username, hashedPass)
         this.userList.push(tmp)
         return tmp.key
     }
+
+    //make some user for testing and for bot
+    initUser(){
+        const hashed = (password) => {return crypto.createHash("sha256").update(password).digest('hex')}
+        //admin
+        this.makeUser('admin', hashed('admin'))
+
+        //some bots
+        this.makeUser('bot1', hashed('bot1'))
+        this.makeUser('bot2', hashed('bot2'))
+    }
+
+
+    //get key from username. Again, this is a bad idea, only fit for testing and demo
+    getKey(username){
+        this.userList.forEach(function(value) {
+            if(value.username == username) return value.key
+        })
+        return null
+    }
+
+    //init some money for testing
+    freeMoney(username, blockchain, amount){
+        key = this.getKey(username)
+        blockchain.freeMoney(key.getPublic('hex'), amount)
+    }
+
 }
+
 
 module.exports.UserFactory = UserFactory
