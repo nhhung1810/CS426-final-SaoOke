7f66c6fc898590d0ebfdd0787bd5bd2ece51aa76
kdnminh-wolfris <khaudangnhatminh@gmail.com>
2021-08-21 13:24:02 +0700

    add already have an account line

--- Client/app/src/main/java/com/example/blockchainapp/RegisterActivity.java
+++ Client/app/src/main/java/com/example/blockchainapp/RegisterActivity.java
@@ -1,8 +1,19 @@
 package com.example.blockchainapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +21,31 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        alreadyHaveAccount();
+    }
+
+    private void alreadyHaveAccount() {
+        SpannableString ss = new SpannableString("Already have an account? Log in");
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+//                startActivity(new Intent(this, LogInActivity.class));
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+            }
+        };
+
+        ss.setSpan(clickableSpan, 25, 31, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
+        TextView textView = findViewById(R.id.tv_login);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setHighlightColor(Color.TRANSPARENT);
     }
 }
