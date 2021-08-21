5f37235fa63661e83a7a41a2ac8f333bb15edf53
FinisTP <tanphat.cineart@gmail.com>
2021-08-21 23:01:12 +0700

    replace ip

--- Client/app/src/main/java/com/example/blockchainapp/BlockchainInterface.java
+++ Client/app/src/main/java/com/example/blockchainapp/BlockchainInterface.java
@@ -4,6 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface BlockchainInterface {
 
@@ -11,7 +12,7 @@ public interface BlockchainInterface {
     Call<Boolean> ExecutePostTransaction(@Body Transaction transaction);
 
     @GET("/balance")
-    Call<Float> ExecuteGetBalance(@Body String privateKey);
+    Call<Float> ExecuteGetBalance(@Query("key") String privateKey);
 
     @POST("/register")
     Call<UserKey> ExecutePostRegister(@Body UserAccount account);
