73b647a54f44212eb9473ebc68dbc9278e9d7114
kdnminh-wolfris <khaudangnhatminh@gmail.com>
2021-08-21 11:36:05 +0700

    register UI

--- Client/app/src/main/java/com/example/blockchainapp/RegisterActivity.java
+++ Client/app/src/main/java/com/example/blockchainapp/RegisterActivity.java
@@ -2,17 +2,13 @@ package com.example.blockchainapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_register);
     }
 }
