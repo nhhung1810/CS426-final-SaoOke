eaf32aaa7fbc9dbc774296940da6156021da8426
nhhung1810 <59091317+yauangon@users.noreply.github.com>
2021-09-03 16:10:30 +0700

    Clean up, add write to pem file

--- Test-Sign/test/src/RSAkey.java
+++ Test-Sign/test/src/RSAkey.java
@@ -11,11 +11,12 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
-import java.util.function.ObjIntConsumer;
 import java.security.KeyPairGenerator;
 import java.security.KeyPair;
 import java.security.SecureRandom;
 import java.security.Signature;
+import java.io.FileNotFoundException;
+import java.security.Key;
 
 import org.json.JSONObject;
 
@@ -29,7 +30,58 @@ class RSAKey{
         return pair;
     }
 
+    private static void writePemFile(Key key, String description, String filename)
+    throws FileNotFoundException, IOException {
+        PemFile pemFile = new PemFile(key, description);
+        pemFile.write(filename);
+    }
+
+    public static void writePemFile(KeyPair keyPair, String name){
+        String publicPath = name + "-public-key.pem";
+        String privatePath = name + "-private-key.pem";
+        try {
+            writePemFile(keyPair.getPrivate(), "PRIVATE KEY", privatePath);
+            writePemFile(keyPair.getPublic(), "PUBLIC KEY", publicPath);
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e.toString());
+        }
+    }
+
     //require 2 file pem
+    private static KeyPair parseKey(String publicPath, String privatePath) throws Exception{
+        String privateKeyContent = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource(privatePath).toURI())));
+        String publicKeyContent = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource(publicPath).toURI())));
+        
+        privateKeyContent = privateKeyContent.replaceAll("\\n", "").replaceAll("\r", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "");
+        publicKeyContent = publicKeyContent.replaceAll("\\n", "").replaceAll("\r", "").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "");;
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+
+        PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent));
+        PrivateKey privKey = kf.generatePrivate(keySpecPKCS8);
+
+        X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKeyContent));
+        RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(keySpecX509);
+        
+        System.out.println(privKey);
+        System.out.println(pubKey);
+
+        return new KeyPair(pubKey, privKey);
+    }
+    
+    public static KeyPair parseKey(String name){
+        String publicPath = name + "-public-key.pem";
+        String privatePath = name + "-private-key.pem";
+        try {
+            return parseKey(publicPath, privatePath);
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
+    //only for testing
     public static KeyPair parseKey() throws InvalidKeySpecException, NoSuchAlgorithmException, IOException, URISyntaxException {
 
         String privateKeyContent = new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource("private_key_pkcs8.pem").toURI())));
@@ -84,25 +136,20 @@ class RSAKey{
     
     public static void main(String[] args){
         KeyPair kp = null;
-        try{
-            // kp = generateKeyPair();
-            kp = parseKey();
-            
-        } catch (Exception ex){
-            System.out.println(ex.toString());;
+        try {
+            kp = generateKeyPair();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e.toString());
         }
-        try{
-            String signature = sign("Hello", kp.getPrivate());
-            System.out.println(signature);
-            System.out.println("\n\n");
-            if(verify("Hello", signature, kp.getPublic()))
-            System.out.println("True msg");
-            else
-            System.out.println("Wrong msg");
-        } catch (Exception ex){
-            System.out.println(ex.toString());
+        writePemFile(kp, "hung");
+        
+        //The key need to be move into the src before ready to parse
+        //t khong biet trong android m se luu sao nen la m check code roi adapt lai nha
+        // try{
+        //     kp = parseKey("hung");
+        // } catch(Exception ex){
+        //     System.out.println(ex.toString());
         }
-        
-        
     }
 }
