2f63c9dc40a63dc7f05fe7f650f07812e4b104c5
kdnminh-wolfris <khaudangnhatminh@gmail.com>
2021-09-05 22:05:53 +0700

    set up sprites and activities

---
+++ Client/app/src/main/java/com/example/blockchainapp/LandingScreen/LandingScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.blockchainapp.LandingScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.blockchainapp.R;
+
+public class LandingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_screen);
+    }
+}
