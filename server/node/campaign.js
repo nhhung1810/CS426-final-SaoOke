b3c4f5f38a4305f3ffc2628dcd2dfd748d0c42d2
cuongnguyenngoc <ngoccuongpvc@gmail.com>
2021-09-05 14:41:24 +0700

    create campaign

---
+++ server/node/campaign.js
@@ -0,0 +1,77 @@
+
+class Campaign {
+    constructor(campaignName, ownerKey, ownerName, targetAmount, expireDate, message) {
+        this.campaignName = campaignName
+        this.ownerKey = ownerKey
+        this.ownerName = ownerName
+        this.targetAmount = targetAmount
+        this.expireDate = expireDate
+        this.message = message
+        this.transactions = []
+    }
+
+    getReceivedAmount() {
+        amount = 0;
+        this.transactions.forEach(transaction => {
+            if (this.ownerKey == transaction.toAddress) {
+                amount += transaction.amount;
+            } else {
+                amount -= transaction.amount;
+            }
+        });
+        return amount;
+    }
+}
+
+class CampaignFactory {
+    constructor() {
+        this.campaignList = []
+    }
+
+    createCampaign(campaignName, ownerKey, ownerName, targetAmount, expireDate, message) {
+        this.campaignList.forEach(element => {
+            if (element.campaignName == campaignName) {
+                throw new Error("This campain name is exists")
+            }
+        });
+        this.campaignList.push(new Campaign(campaignName, ownerKey, ownerName, targetAmount, expireDate, message))
+    }
+
+    donate(donatorKey, campaignName, amount, message) {
+        targetCampain = null
+        this.campaignList.forEach(campaign => {
+            if (campaign.campaignName == campaignName) {
+                targetCampain = campaign
+                break
+            }
+        });
+        if (targetCampain == null) {
+            throw new Error("Campaign not found");
+        }
+
+        // make new transaction here
+        
+        //
+    }
+
+    give(ownerKey, receiverKey, amount, message) {
+
+        //make new transaction here
+        //
+    }
+
+    getCampaignInformation(campaignName) {
+        this.campaignList.forEach(element => {
+            if (element.campaignName == campaignName) {
+                return {
+                    "campaignName" : element.campaignName,
+                    "ownerKey" : element.ownerKey,
+                    "ownerName" : element.ownerName,
+                    "targetAmount" : element.targetAmount,
+                    "expireDate" : element.expireDate,
+                    "total_amount" : element.getReceivedAmount
+                }
+            }
+        });
+    }
+}
